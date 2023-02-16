d430920a56aaa3850994f0b07d1e77d8d3f22709
Federicovedelini <vedelinifede10@gmail.com>
2023-02-15 21:51:45 -0300

    servidor express terminado

--- src/server.js
+++ src/server.js
@@ -6,10 +6,24 @@ const app = express();
 app.use(express.urlencoded({extended: true}));
 
 const productos = new ProductManager()
-const rearProducts = productos.readProducts()
+const readProducts = productos.readProducts()
 
 app.get ("/products", async (req,res) => {
-   console.log(await rearProducts);
+
+    let limit = parseInt(req.query.limit);
+    if(!limit) return res.send(await readProducts)
+    let allProducts = await readProducts
+    let productLimit = allProducts.slice(0, limit)
+   res.send(productLimit);
+
+});
+
+app.get("/products/:id", async (req,res) => {
+    let id = parseInt(req.params.id);
+    let allProducts = await readProducts;
+    let productById = allProducts.find(product => product.id === id)
+    res.send(productById);
+
 
 });
 
